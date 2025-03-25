f67b4cf90650aba030bb340500caf02deae1a853
maxgrin04 <mgrin98@gmail.com>
2025-03-26 00:42:56 +0900

    Step 6

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,8 @@ import pytest
 
 
 @pytest.mark.parametrize("file_path1, file_path2, expected, format_name", [
-    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_1.txt', 'plain')
+    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_1.txt', 'plain'),
+    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml', 'tests/test_data/expected_result_1.txt', 'plain')
 ])
 def test_generate_diff(file_path1, file_path2, expected, format_name):
     with open(expected,) as file:
