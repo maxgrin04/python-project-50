3570fb4158155a78b26a0b305d8028da5525ce90
maxgrin04 <mgrin98@gmail.com>
2025-03-28 05:10:04 +0900

    Step 9

--- gendiff/arg_parse.py
+++ gendiff/arg_parse.py
@@ -1,6 +1,7 @@
 import argparse
 from gendiff.diff import generate_diff
 
+
 def arg_parse():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
